3788291592d62e6a6fb84118290bbe3c7bcd03c7
Sude Nur Taylan <staylan@k1m34s02.42istanbul.com.tr>
2025-06-24 18:54:29 +0300

    baslangıc

---
+++ philo.c
@@ -0,0 +1,152 @@
+#include "philo.h"
+
+int create_philos()
+{
+
+}
+
+
+int create_threads()
+{
+
+}
+
+int philo_routines()
+{
+//eat sleep think
+
+}
+
+int arg_control(char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    while(argv[i])
+    {
+        j = 0;
+        while(argv[i][j])
+        {
+            if(argv[i][j] == '-')
+                j++;
+            if(argv[i][j] <= '9' && argv[i][j] >= '0')
+                ;
+            else
+                return 0;
+            j++;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	res;
+
+	sign = 1;
+	res = 0;
+	while (*str == '\t' || *str == '\n' || *str == '\v'
+		|| *str == '\f' || *str == '\r' || *str == ' ')
+		str++;
+	if (*str == '-')
+	{
+		sign *= -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	if (*str == '-' || *str == '+')
+		return (0);
+	while (*str >= '0' && *str <= '9')
+	{
+		res = (res * 10);
+		res = res + (*str - '0');
+		str++;
+	}
+	return (res * sign);
+}
+
+int init_tdata(char **argv, t_data *data, int argc)
+{
+    int i;
+
+    i = 1;
+    while(argv[i])
+    {
+        if(ft_atoi(argv[i]) < 0)
+            return 0;
+        i++;
+    }
+    data->number_of_philos = ft_atoi(argv[1]);
+    data->time_to_die = ft_atoi(argv[2]);
+    data->time_to_eat = ft_atoi(argv[3]);
+    data->time_to_sleep = ft_atoi(argv[4]);
+    if(argc == 6)
+        data->number_of_eat = ft_atoi(argv[5]);
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->number_of_philos);
+    if(data->forks == NULL)
+    {
+        //free
+        return 0;
+    }
+    if(pthread_mutex_init(&data->print_lock, NULL) != 0)
+    {
+        free(data->forks);
+        return 0;
+    }
+    i = 0;
+    while (i < data->number_of_philos) 
+    {
+        if (pthread_mutex_init(&data->forks[i], NULL) != 0) {
+            while (--i >= 0)
+                pthread_mutex_destroy(&data->forks[i]);
+            pthread_mutex_destroy(&data->print_lock);
+            free(data->forks);
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int init_tphilo(t_data *data, t_philo **philo)
+{
+    int i;
+
+    i = 0;
+    philo = malloc(sizeof(t_philo) * data->number_of_philos);
+    if(philo == NULL)
+    {
+        //free
+        return 0;        
+    }
+    while(i < data->number_of_philos)
+    {
+        philo[i]->id = i + 1;
+        philo[i]->meals_eaten = 0;
+        philo[i]->last_meal_time = 0;
+        philo[i]->data = &data;
+        philo[i]->l_fork = i;
+        philo[i]->r_fork = (i + 1) % data->number_of_philos;
+        i++;            
+    }
+    return 1;
+}
+int main(int argc, char **argv)
+{
+    t_data data;
+    t_philo *philo;
+
+    if(argc != 6 && argc != 5)
+        return 1;
+    if(!arg_control(argv))
+        return 1;
+    if(!init_tdata(argv, &data, argc))
+        return 1;
+    if(!init_tphilo(&data, &philo))
+        return 1;
+
+}
---
+++ philo.h
@@ -0,0 +1,29 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct s_data{
+    int number_of_philos;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_eat;
+    long long start_time;
+    pthread_mutex_t *forks;
+    pthread_mutex_t print_lock;
+}t_data;
+
+typedef struct s_philo{
+    int id;
+    int meals_eaten;
+    int r_fork;
+    int l_fork;
+    t_data *data;
+    pthread_t thread;
+    long long last_meal_time;
+
+}t_philo;
+
+#endif
